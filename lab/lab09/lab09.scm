188de265be1db6195a032c8d2dbaffaeabc880f7
paullok999 <paulvon369@Outlook.com>
2024-07-17 17:16:52 +0800

    Finish lab09

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,10 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (if (< num1 num2) -1 (if (> num1 num2) 1 0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (if (= n 1) f (lambda (x) (f ((repeat f (- n 1)) x)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +16,4 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (if (zero? b) a (gcd b (modulo a b))))
