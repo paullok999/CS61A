5bf04ac46cecf35f3b310f06528dad377567dd2a
paullok999 <paulvon369@Outlook.com>
2024-07-24 13:54:33 +0800

    Finish hw08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -9,14 +9,34 @@
 
 
 (define (my-filter pred s) 
-    (if (null? s) '()
-        (if (pred (car s)) 
-            (cons (car s) (my-filter pred (cdr s)))
-            (my-filter pred (cdr s))
-        )
-    )
+ (if (null? s) '()
+  (if (pred (car s)) 
+   (cons (car s) (my-filter pred (cdr s)))
+   (my-filter pred (cdr s))
+  )
+ )
 )
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+ (cond 
+  ((null? lst1) lst2)
+  ((null? lst2) lst1)
+  (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))
+ )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+ (cond 
+  ((null? s) '())
+  ((= 1 (ops s (car s))) (cons (car s) (no-repeats (cdr s))))
+  (else (no-repeats (cdr s)))
+ )
+)
+
+(define (ops s x)
+ (cond 
+  ((null? s) 0)
+  ((= (car s) x) (+ 1 (ops (cdr s) x)))
+  (else (ops (cdr s) x))
+ )
+)
